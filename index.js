762e2152b34780e97c9fb6ea7e48bd71b26aa751
Anthony Febus <febus.anthony@gmail.com>
2018-08-12 16:49:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions and code here
+// kittens string
+ var kittens = ["Milo", "Otis", "Garfield"];
+ 
+function destructivelyAppendKitten (kittensString) {
+  kittens.push(kittensString);
+    return kittens;
+}
+
+function destructivelyPrependKitten (kittensString) {
+  kittens.unshift(kittensString);
+    return kittens;
+}
+
+function destructivelyRemoveLastKitten (kittensString) {
+  kittens.pop(kittensString);
+    return kittens;
+}
+
+function destructivelyRemoveFirstKitten (kittensString) {
+  kittens.shift(kittensString);
+    return kittens;
+}
